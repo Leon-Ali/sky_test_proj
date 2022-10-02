5e5237623e49c3199e31e9fb80796f9562356ae2
lom-ali yakubov <lomaliyakubov@gmail.com>
2022-10-02 12:28:41 +0300

    Add Resume model and requirements file

--- headhunter/models.py
+++ headhunter/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class ResumeStatus(models.TextChoices):
+    DRAFT = 'DR'
+    PUBLISHED = 'PB'
+
+
+class Grade(models.TextChoices):
+    JUNIOR = 'JUN'
+    MIDDLE = 'MID'
+    SENIOR = 'SEN'
+
+
+class Resume(models.Model):
+
+    user = models.OneToOneField(User, verbose_name='пользователь')
+    status = models.CharField(
+        max_length=2,
+        choices=ResumeStatus.choices,
+        default=ResumeStatus.DRAFT,
+        verbose_name='статус',
+    )
+    grade = models.CharField(
+        max_length=3,
+        choices=Grade.choices,
+        verbose_name='грейд',
+    )
+    specialty = models.CharField(max_length=250, verbose_name='специальность')
+    salary = models.FloatField(verbose_name='зарплата')
+    education = models.TextField(verbose_name='образование')
+    experience = models.TextField(verbose_name='опыт')
+    portfolio = models.URLField(verbose_name='портфолио')
+    title = models.CharField(max_length=250, verbose_name='название')
+    phone = PhoneNumberField(blank=True, verbose_name='телефон')
+    email = models.EmailField(verbose_name='email')
